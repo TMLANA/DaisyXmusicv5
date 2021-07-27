9acc5d440435221c78d25b24219962b24c3f2897
TMLANA <83625300+TMLANA@users.noreply.github.com>
2021-07-27 13:47:45 +0300

    Update song.py

--- DaisyXMusic/modules/song.py
+++ DaisyXMusic/modules/song.py
@@ -76,7 +76,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by Haguromo Otsusuki**"
+        rep = "**🎵 اۏنـــ͜ާﻶﯾڼ**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -91,7 +91,7 @@ def song(client, message):
         )
         m.delete()
     except Exception as e:
-        m.edit("❌ Error")
+        m.edit("❌ خطاء)
         print(e)
 
     try:
